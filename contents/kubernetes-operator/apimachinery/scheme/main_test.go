07a7e3bb2ac98cf6d7e23a81f8fa6cac1556aad7
Masato Naka <masatonaka1989@gmail.com>
2022-07-30 21:29:53 +0900

    update

---
+++ contents/kubernetes-operator/apimachinery/scheme/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+	scheme "github.com/nakamasato/kubernetes-training/kubernetes-operator/apimachinery/scheme/scheme"
+)
+
+func TestScheme(t *testing.T) {
+	typesNum := len(scheme.Scheme.AllKnownTypes())
+	if typesNum != 31 {
+		t.Errorf("AllKnownTypes want 31, got %d\n", typesNum) // appsv1 has 31 types
+	}
+}
